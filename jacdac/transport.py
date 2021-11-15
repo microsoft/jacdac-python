00b1b4b416b526d2085a6284d4af01ae4a521bd2
pelikhan <jhalleux@microsoft.com>
2021-11-15 15:40:21 -0800

    fix: trying to trigger build

--- jacdac/transport.py
+++ jacdac/transport.py
@@ -5,6 +5,7 @@ class Transport:
     # A base class for packet transports
 
     on_receive: Optional[Callable[[bytes], None]] = None
+        # Callback to report a received packet to the bus
 
     def send(self, pkt: bytes) -> None:
         # send a packet payload over the transport layer

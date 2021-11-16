5fe8f266d828f1b8f16eac9dad0ba6f714fccf72
pelikhan <jhalleux@microsoft.com>
2021-11-15 16:38:55 -0800

    fix: use setup.py cos you have to

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+__version__ = "0.0.1"
+
+setup(
+    name="jacdac",
+    version=__version__,
+    description="Python client package for Microsoft Jacdac",
+    license="MIT",
+    keyboards=["jacdac"],
+    author="Microsoft Corporation",
+    url="https://microsoft.github.io/jacdac-docs/clients/python/"
+)

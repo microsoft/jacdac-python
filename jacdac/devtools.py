af69c7226a79c202c1059920d6933605e3e238b7
pelikhan <jhalleux@microsoft.com>
2021-11-15 17:34:38 -0800

    move devtools up

--- jacdac/devtools.py
+++ jacdac/devtools.py
@@ -1,5 +1,5 @@
-from jacdac.bus import Bus
-from jacdac.transports.ws import WebSocketTransport
+from .bus import Bus
+from .transports.ws import WebSocketTransport
 
 print("open http://localhost:8081 to diagnose python stack")
 transport = WebSocketTransport("ws://localhost:8081")

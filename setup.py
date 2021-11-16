6a46745ad2e6fe56ccdc6b2473492876d6b18a9f
pelikhan <jhalleux@microsoft.com>
2021-11-15 17:09:25 -0800

    fix: moving metadata to .cfg

--- setup.py
+++ setup.py
@@ -3,33 +3,5 @@ from setuptools import setup
 __version__ = "0.0.4"
 
 setup(
-    name="pyJacdac",
     version=__version__,
-    description="Python client package for Microsoft Jacdac",
-    license="MIT",
-    keyboards=["jacdac"],
-    author="Microsoft Corporation",
-    url="https://microsoft.github.io/jacdac-docs/clients/python/",
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
-    classifiers=[
-        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: Unix',
-        'Operating System :: POSIX',
-        'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: System :: Hardware',
-        'Topic :: Software Development :: Embedded Systems'
-    ],
 )

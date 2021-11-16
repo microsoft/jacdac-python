3bfc20edcbee720ec9c079e6d152b470c037f611
pelikhan <jhalleux@microsoft.com>
2021-11-16 13:55:49 -0800

    fix button import

--- jacdac/button/client.py
+++ jacdac/button/client.py
@@ -4,4 +4,4 @@ from .constants import *
 
 class ButtonClient(Client):
     def __init__(self, bus: Bus, role: str) -> None:
-        super().__init__(bus, SDFSDF, role)
+        super().__init__(bus, JD_SERVICE_CLASS_BUTTON, role)

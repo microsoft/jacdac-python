31348322f354609b1d30cfebf22536b97ed85e36
pelikhan <jhalleux@microsoft.com>
2021-11-15 17:49:03 -0800

    added notes

--- jacdac/devtools.py
+++ jacdac/devtools.py
@@ -6,7 +6,9 @@ def on_device_connect(dev: Device):
     print("connected " + dev.short_id)
 
 
-print("open http://localhost:8081 to diagnose python stack")
+print("pyJacdac dev tools")
+print("run scripts/devtools.sh to launch the development server")
+print("open http://localhost:8081 to connect")
 transport = WebSocketTransport("ws://localhost:8081")
 bus = Bus(transport)
 bus.on(EV_DEVICE_CONNECT, on_device_connect)

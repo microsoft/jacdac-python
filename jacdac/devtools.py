1ff7078c07f498d8cd4ccca0f5bcef7c0c319891
pelikhan <jhalleux@microsoft.com>
2021-11-17 12:29:04 -0800

    testing comments

--- jacdac/devtools.py
+++ jacdac/devtools.py
@@ -1,10 +1,14 @@
-from .bus import Bus, Device, EV_DEVICE_CONNECT
+from .bus import Bus
 from .transports.ws import WebSocketTransport
 
 
 def create_dev_tools_bus():
-    # Starts a Jacdac bus connected to the local detools websocket server
-    # at ws://localhost:8081
+    """Starts a Jacdac bus connected to the local detools websocket server
+    at ws://localhost:8081 .
+
+    Returns:
+        Bus: Jacdac bus using local websocket
+    """
     print("jacdac-python dev tools")
     print("run scripts/devtools.sh to launch the development server")
     print("open http://localhost:8081 to connect")

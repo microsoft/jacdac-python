b6db6342b58e3c6806ed4bcb1c32c29c9a762512
pelikhan <jhalleux@microsoft.com>
2021-11-17 11:39:29 -0800

    generate packformats constants

--- jacdac/color/constants.py
+++ jacdac/color/constants.py
@@ -3,3 +3,6 @@ from jacdac.constants import *
 from jacdac.system.constants import *
 JD_SERVICE_CLASS_COLOR = const(0x1630d567)
 JD_COLOR_REG_COLOR = const(JD_REG_READING)
+JD_COLOR__PACK_FORMATS = {
+    JD_COLOR_REG_COLOR: "u0.16 u0.16 u0.16"
+}
--- jacdac/dmx/constants.py
+++ jacdac/dmx/constants.py
@@ -4,3 +4,7 @@ from jacdac.system.constants import *
 JD_SERVICE_CLASS_DMX = const(0x11cf8c05)
 JD_DMX_REG_ENABLED = const(JD_REG_INTENSITY)
 JD_DMX_CMD_SEND = const(0x80)
+JD_DMX__PACK_FORMATS = {
+    JD_DMX_REG_ENABLED: "u8",
+    JD_DMX_CMD_SEND: "b"
+}

d119dbfe23cb0698aa83aa8620623cdae845e3bb
pelikhan <jhalleux@microsoft.com>
2021-11-18 16:34:02 -0800

    ignore constants files

--- docs/conf.py
+++ docs/conf.py
@@ -48,6 +48,7 @@ intersphinx_disabled_domains = ['std']
 
 autoapi_type = 'python'
 autoapi_dirs = ['../jacdac']
+autoapi_ignore = ['*migrations*', '*constants.py']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']

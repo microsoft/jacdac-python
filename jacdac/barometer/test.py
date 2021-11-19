f2c8a1336fdae335aa08b5c85e29d684dc56117b
pelikhan <jhalleux@microsoft.com>
2021-11-18 21:47:56 -0800

    fix: cleanup test

--- jacdac/barometer/test.py
+++ jacdac/barometer/test.py
@@ -6,15 +6,6 @@ from time import sleep
 if __name__ == '__main__':
     bus = create_dev_tools_bus()
 
-    def up(data: Any):
-        print("up")
-
-    def down(data: Any):
-        print("down")
-
-    def hold(data: list[Type[int]]):
-        print("hold", data[0])
-
     barometer = BarometerClient(bus, "barometer")
     while True:
         print("pressure: ", barometer.pressure, "e:", barometer.pressure_error)

29524b7a3a2c18093c7e577cc09dcf7be35eb739
pelikhan <jhalleux@microsoft.com>
2021-11-20 00:21:29 -0800

    ignore tests in docs

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,8 @@ intersphinx_disabled_domains = ['std']
 
 autoapi_type = 'python'
 autoapi_dirs = ['../jacdac']
-autoapi_ignore = ['*migrations*', '*constants.py', '*transports/*.py']
+autoapi_ignore = ['*migrations*', '*constants.py',
+                  '*test.py', '*transports/*.py']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']

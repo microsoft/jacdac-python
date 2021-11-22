26de946cf24730920a9b8ba42895b0822071621f
pelikhan <jhalleux@microsoft.com>
2021-11-22 15:08:30 -0800

    fix: move setup to .py

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from jacdac.constants import JD_VERSION
 
 setup(
     version=JD_VERSION,
+    packages=find_packages(
+        include=['jacdac', 'jacdac.*'], exclude=['*test.py'])
+
 )

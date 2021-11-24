bde68faa543755d8c325661ce70d3094df2a380c
pelikhan <jhalleux@microsoft.com>
2021-11-23 16:25:13 -0800

    updated samples

--- examples/slidy_blinky.py
+++ examples/slidy_blinky.py
@@ -1,15 +1,19 @@
 from jacdac.devtools import create_dev_tools_bus
 from jacdac.led import LedClient
+from jacdac.potentiometer import PotentiometerClient
 from time import sleep
 
 if __name__ == '__main__':
     def main():
         bus = create_dev_tools_bus()
         led = LedClient(bus, "led")
-        speed = 0
-        brightness = 0xfa
+        slider = PotentiometerClient(bus, "slider")
+        speed = 16
+        brightness = 128
         # fade between colors
         while True:
+            position = slider.position or 0.
+            brightness = int(position / 100. * 255.)
             # blue
             led.animate(0, 0, brightness, speed)
             sleep(1)

ecd5aa74fbb01cf87b6835cdde33ea1116e47733
pelikhan <jhalleux@microsoft.com>
2022-02-01 15:09:43 -0800

    more error handling

--- jacdac/transports/spi.py
+++ jacdac/transports/spi.py
@@ -3,10 +3,9 @@ from typing import List, Optional
 from time import sleep
 from tokenize import Number
 from jacdac.bus import Transport
-from jacdac.util import buf2hex, hex2buf
+from jacdac.util import buf2hex, hex2buf, now
 from gpiod import Chip, Line, LineBulk, LINE_REQ_EV_RISING_EDGE, LINE_REQ_FLAG_ACTIVE_LOW, LINE_REQ_DIR_OUT
 from spidev import SpiDev
-from jacdac.util import buf2hex, hex2buf
 
 XFER_SIZE = 256
 # https://git.kernel.org/pub/scm/libs/libgpiod/libgpiod.git/tree/bindings/python/gpiodmodule.c?h=v1.6.x&id=27cacfe377114f6acf67cd943d1ca01bb30e0f2b
@@ -15,6 +14,7 @@ RPI_PIN_TX_READY = 24
 RPI_PIN_RX_READY = 25
 RPI_PIN_RST = 22
 CONSUMER = "jacdac"
+MAX_SEND_QUEUE_LEN = 10
 
 class SpiTransport(Transport):
     def __init__(self):
@@ -65,37 +65,42 @@ class SpiTransport(Transport):
                 print("error: device failed to close")
         
     def _flip_reset(self) -> None:
-        print("spi: flip reset")
+        print("spi: reset bridge")
+        self.sendQueue = []
         rst = self.chip.get_line(RPI_PIN_RST)
         try:
-            rst.request(consumer = CONSUMER, type = LINE_REQ_DIR_OUT, flags = LINE_REQ_FLAG_ACTIVE_LOW)
+            rst.request(consumer = CONSUMER, type = LINE_REQ_DIR_OUT)
             rst.set_value(0)
             sleep(0.01)
             rst.set_value(1)
-            rst.set_direction_input()
         finally:
             rst.release()
 
     def send(self, pkt: bytes) -> None:
-        print("spi: send " + buf2hex(pkt))
         self.sendQueue.append(pkt)
         self._transfer()
 
     def _read_loop(self) -> None:
-        while True:
-            ev_lines = self.rxtx.event_wait(nsec = 1) # List[Line]
-            if ev_lines:
-                self._transfer()
+        try:
+            while True:
+                ev_lines = self.rxtx.event_wait(nsec = 1) # List[Line]
+                if ev_lines:
+                    self._transfer()
+        except:
+            self.close()
+            raise
 
     def _read_ready_pins(self) -> List[int]:
         rxtxv = self.rxtx.get_values() 
         if rxtxv is None:
             return [0, 0]
         else:
-            print(rxtxv)
             return rxtxv
 
     def _transfer(self) -> None:
+        if len(self.sendQueue) > MAX_SEND_QUEUE_LEN:
+            self._flip_reset()
+
         try:
             while self._transfer_frame():
                 pass
@@ -109,11 +114,10 @@ class SpiTransport(Transport):
         txReady = tx != 0
         sendtx = txReady and len(self.sendQueue) > 0
 
-        print("spi: transfer rx:" + str(rx) + ", tx: " + str(tx) + ", queue: " + str(len(self.sendQueue)))
+      #  print("spi: transfer rx:" + str(rx) + ", tx: " + str(tx) + ", queue: " + str(len(self.sendQueue)))
 
         if not sendtx and not rxReady:
             return False
-
 
         # allocate transfer buffers
         txqueue = bytearray(XFER_SIZE)
@@ -123,7 +127,7 @@ class SpiTransport(Transport):
         while len(self.sendQueue) > 0:
             pkt = self.sendQueue[0]
             npkt = len(pkt)
-            if txq_ptr + npkt > XFER_SIZE:
+            if txq_ptr + npkt > len(txqueue):
                 break
             self.sendQueue.pop(0)
             txqueue[txq_ptr:txq_ptr+npkt] = pkt
@@ -132,32 +136,42 @@ class SpiTransport(Transport):
         if txq_ptr == 0 and not rxReady:
             return False
 
-        print("spi: send frame " + buf2hex(txqueue))
-        rxqueue = bytearray(self.spi.xfer(txqueue))
-        print("recv " + buf2hex(rxqueue))
+        rxqueue = bytearray(0)
+        if txq_ptr > 0:
+            txqueue = bytearray(txqueue[0::txq_ptr])
+            print(str(now()) + " " + buf2hex(txqueue) + " send frame")
+            rxqueue = bytearray(self.spi.xfer(txqueue))
+        elif rxReady:
+            rxqueue = bytearray(self.spi.readbytes(XFER_SIZE))
         if rxReady:
             if rxqueue is None:
                 print("recv failed")
                 return False
+            #print(str(now()) + " " + buf2hex(rxqueue) + " recv frame")
             
             framep = 0
-            while framep + 4 < XFER_SIZE :
+            while framep + 4 < len(rxqueue) :
                 frame2 = rxqueue[framep + 2]
                 if frame2 == 0:
+                    print("spi: empty frame")
                     break
                 sz = frame2 + 12
-                if framep + sz > XFER_SIZE:
+                if framep + sz > len(rxqueue):
+                    print("spi: frame size out of range")
                     break
                 frame0 = rxqueue[framep]
                 frame1 = rxqueue[framep + 1]
                 frame3 = rxqueue[framep + 3]
                 if frame0 == 0xff and frame1 == 0xff and frame3 == 0xff :
                     # skip bogus packet
+                    print("spi: skip bogus pkt")
                     pass
                 else:
                     buf = bytearray(rxqueue[framep:framep+sz])
+                    print("spi: recv pkt " + buf2hex(buf))
                     if buf and self.on_receive:
                         self.on_receive(buf)
                 sz = (sz + 3) & ~3
                 framep += sz
+        # and we're done
         return True
